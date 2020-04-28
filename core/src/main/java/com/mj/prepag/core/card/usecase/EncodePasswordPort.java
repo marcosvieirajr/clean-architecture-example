36ce6d5622974a5b2464917248edea28eabb0bd8
Marcos Vieira Jr <marcosvieirajr@gmail.com>
2020-04-28 03:12:42 -0300

    change maven to gradle project

--- core/src/main/java/com/mj/prepag/core/card/usecase/EncodePasswordPort.java
+++
@@ -1,7 +0,0 @@
-package com.mj.prepag.core.card.usecase;
-
-public interface EncodePasswordPort {
-	
-	String encode(String str);
-
-}
--- core/src/main/java/com/mj/prepag/core/card/usecase/SaveCardPort.java
+++
@@ -1,8 +0,0 @@
-package com.mj.prepag.core.card.usecase;
-
-import com.mj.prepag.core.card.entities.Card;
-
-public interface SaveCardPort {
-
-	Card save(Card card);
-}
